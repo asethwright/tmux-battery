3e8140277c99d4526f5783af0e854029e32747d3
Jan Ahrens <jan.ahrens+git@googlemail.com>
2015-02-27 07:20:14 +0100

    Use dbus-send to query for remaining time for non-OSX
    Unfortunately `upower` does not return the remaining time in seconds and
    instead uses formatted strings like "42 minutes". This makes it hard to
    add the time of many batteries together. Fortunately UPower also
    provides a dbus device that can be queried using dbus-send.
    
    I had to write some helper functions to make this work. This increased
    the complexity compared to the previous solution. That's why I decided
    to also write a minimal test framework to ensure that they can be
    refactored safely.

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -17,3 +17,47 @@ command_exists() {
 	local command="$1"
 	type "$command" >/dev/null 2>&1
 }
+
+format_time() {
+	seconds=$1
+	((h=${seconds}/3600))
+	((m=(${seconds}%3600)/60))
+	((s=${seconds}%60))
+	printf "%02d:%02d\n" $h $m
+}
+
+# Takes integers separated by newlines and outputs the sum
+average() {
+	sum=0
+	number_of_values=0
+	for value in $@; do
+		if (($value > 0)); then
+			((sum=$sum+$value))
+			((number_of_values=$number_of_values+1))
+		fi
+	done
+	((sum=$sum/$number_of_values))
+	echo $sum
+}
+
+# http://upower.freedesktop.org/docs/Device.html
+dbus_value() {
+	battery=$1
+	value=$2
+	dbus-send --print-reply=literal --system \
+		--dest=org.freedesktop.UPower $battery \
+		org.freedesktop.DBus.Properties.Get string:org.freedesktop.UPower \
+		$value
+}
+
+upower_time_to_empty() {
+	for battery in $(upower -e | grep battery); do
+		dbus_value $battery string:TimeToEmpty | awk '{print $3}'
+	done
+}
+
+upower_time_to_full() {
+	for battery in $(upower -e | grep battery); do
+		dbus_value $battery string:TimeToFull | awk '{print $3}'
+	done
+}
